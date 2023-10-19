1e982353cf3496972e67d19ff36f1c496ccc5bad
Mark Clearwater <csmacnz@csmac.co.nz>
2023-10-20 11:39:34 +1300

    Added contact page

---
+++ dotnet-demo/Controllers/ContactsController.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using dotnet_demo.Models;
+using Htmx;
+
+namespace dotnet_demo.Controllers;
+
+public class ContactsController : Controller
+{
+    private readonly ILogger<ContactsController> _logger;
+
+    public ContactsController(ILogger<ContactsController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        if (Request.IsHtmx())
+        {
+            // When we respond to HTMX
+            return PartialView();
+        }
+
+        return View();
+    }
+
+    public IActionResult ViewContact()
+    {
+        return View();
+    }
+
+    public IActionResult EditContact()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
